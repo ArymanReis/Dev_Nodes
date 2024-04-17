a3270d430d86f8434ce5069326e77e9aa6d9ce73
Aryman Reis <arymanreis@gmail.com>
2024-04-17 11:40:54 -0300

    Adicionando eventos
    Adicionando eventos e funções

--- js/index.js
+++ js/index.js
@@ -3,15 +3,23 @@ const notesContainer = document.querySelector("#notes-container");
 const noteInput = document.querySelector("#note-content");
 const addNoteBtn = document.querySelector(".add-note");
 
+const searchInput = document.querySelector("#search-input");
+
 // Funções
 function showNotes() {
+  cleanNotes();
+
   getNotes().forEach((note) => {
     const noteElement = createNote(note.id, note.content, note.fixed);
 
     notesContainer.appendChild(noteElement);
   });
-}
-;
+};
+
+function cleanNotes() {
+  notesContainer.replaceChildren([]);
+};
+
 function addNote() {
 
   const notes = getNotes();
@@ -51,12 +59,114 @@ function createNote(id, content, fixed) {
 
   element.appendChild(textarea);
 
+  const pinIcon = document.createElement("i");
+
+  pinIcon.classList.add(...["bi", "bi-pin"]);
+
+  element.appendChild(pinIcon);
+  
+  const deleteIcon = document.createElement("i");
+
+  deleteIcon.classList.add(...["bi", "bi-x-lg"]);
+
+  element.appendChild(deleteIcon);
+
+  const duplicateIcon = document.createElement("i");
+
+  duplicateIcon.classList.add(...["bi", "bi-file-earmark-plus"]);
+
+  element.appendChild(duplicateIcon);
+
+  if(fixed) {
+    element.classList.add("fixed");
+  }
+
+  // Evento do elemento
+  element.querySelector("textarea").addEventListener("keyup", (e) => {
+
+    const noteContent = e.target.value;
+
+    updateNote(id, noteContent);
+
+  });
+
+  element.querySelector(".bi-pin").addEventListener("click", () => {
+    ToggleFixNote(id);
+  });
+
+  element.querySelector(".bi-x-lg").addEventListener("click", () => {
+    deleteNote(id, element)
+  });
+
+  element.querySelector(".bi-file-earmark-plus").addEventListener("click", () => {
+    copyNote(id);
+  });
+
   return element;
-}
+};
+
+function ToggleFixNote(id) {
+  const notes = getNotes();
+
+  const targetNote = notes.filter((note) => note.id === id) [0];
+
+  targetNote.fixed = !targetNote.fixed;
+
+  saveNotes(notes);
+
+  showNotes();
+};
+
+function deleteNote(id, element) {
+
+  const notes = getNotes().filter((note) => note.id !== id);
+
+  saveNotes(notes);
+
+  notesContainer.removeChild(element);
+
+};
+
+function copyNote(id) {
+
+  const notes = getNotes()
+
+  const targetNote = notes.filter((note) => note.id === id)[0];
+
+  const noteObject = {
+    id: generateId(),
+    content: targetNote.content,
+    fixed: false,
+  };
+
+  const noteElement = createNote(noteObject.id, noteObject.content, noteObject.fixed);
+
+  notesContainer.appendChild(noteElement);
+
+  notes.push(noteObject);
+
+  saveNotes(notes);
+
+};
+
+function updateNote(id, newContent) {
+
+  const notes = getNotes();
+
+  const targetNote = notes.filter((note) => note.id === id)[0];
+
+  targetNote.content = newContent;
+
+  saveNotes(notes);
+
+};
+
 
 // Local storage
 function getNotes() {
   const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+
+  const orderedNotes = notes.sort((a, b) => a.fixed > b.fixed ? -1 : 1);
 
   return notes;
 };
@@ -65,10 +175,43 @@ function saveNotes(notes) {
   localStorage.setItem("notes", JSON.stringify(notes));
 };
 
+function searchNotes(search) {
 
+  const searchResults = getNotes().filter((note) => note.content.includes(search)
+  );
 
-// Evento
+  if (search !== "") {
+    cleanNotes();
+
+    searchResults.forEach((note) => {
+      const noteElement = createNote(note.id, note.content);
+      notesContainer.appendChild(noteElement);
+    });
+
+    return;
+  }
+
+  cleanNotes();
+
+  showNotes();
+};
+
+// Eventos
 addNoteBtn.addEventListener("click", () => addNote());
+
+searchInput.addEventListener("keyup", (e) => {
+  const search = e.target.value;
+
+  searchNotes(search);
+});
+
+noteInput.addEventListener("keydown", (e) => {
+
+  if(e.key === "Enter") {
+    addNote();
+  };
+
+});
 
 // Inicialização
 showNotes();
